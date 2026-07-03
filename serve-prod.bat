e6fd92e60c49d229229cb9a55b38702a24a94858
AW GO TRAVEL <sales@awgotravel.com>
2026-07-03 11:31:05 +0200

    chore: add Unsplash remote patterns and dev scripts

---
+++ serve-prod.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\gianc\Documents\Verdent Proyectos\Awgotravel"
+serve out -p 3000
---
+++ start-dev.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\gianc\Documents\Verdent Proyectos\Awgotravel"
+npx next dev --port 3000
